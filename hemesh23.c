ad915f87d800a2abefe053eb2ca22c72ebdfed80
hemesh <118333766+Spy7204@users.noreply.github.com>
2023-11-09 19:59:09 +0530

    Update hemesh23.c

--- hemesh23.c
+++ hemesh23.c
@@ -1,73 +1,52 @@
-include <stdio.h>
-
-// Recursive function to perform merge sort on arr[l..r]
-void msort(int arr[], int l, int r) {
-    if (l < r) {
-        
-        int m = l + (r - l) / 2;    // Middle point
-
-        // "Recursively sort"
-        msort(arr, l, m);               // [1,3,4,2] --> [1,3]  [4,2]  --> [1][3][4][2]
-        msort(arr, m + 1, r);
-
-
-        // "Merge" the sorted halves
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int L[n1], R[n2];   // Create temporary arrays
-
-
-        // "Copy data" to temporary arrays L[] and R[]
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0, k = l;
-
-
-        //--------------  Merge the temporary arrays -----------------------
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-
-        // ---------------- Copy the remaining elements of L[] and R[] -----------------
-        while (i < n1) {
-            arr[k] = L[i];
+//QuickSort
+#include <stdio.h>
+int partition(int arr[],int low,int high){
+    int pivot=arr[low];
+    int i=low+1;
+    int j=high;
+    int temp;
+    do{
+        while(arr[i]<=pivot){
             i++;
-            k++;
         }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        while(arr[j]>pivot){
+            j--;
         }
-    }
+        if(i<j){
+            temp= arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    } while (i<j);
+        temp=arr[low];
+        arr[low]=arr[j];
+        arr[j]=temp;
+        return j;
+
 }
+void QuickSort(int arr[],int low,int high)
+{       int partitionIndex;
+        if(low<high) {
+            partitionIndex=partition(arr, low, high);
+            QuickSort(arr, low, partitionIndex - 1);
+            QuickSort(arr, partitionIndex + 1, high);
+    }
 
+}
 int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Unsorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    int n;
+    printf("Enter the size of the array:-");
+    scanf("%d",&n);
+    int arr[n];
+    for (int k = 0; k <n ; ++k) {
+        printf("Enter the %d element of the array:-",k+1);
+        scanf("%d",&arr[k]);
     }
-
-    msort(arr, 0, n - 1);
- 
-    printf("\nSorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+    printf("Sorted Array:-\n");
+    QuickSort(arr,0,n-1);
+    for (int k = 0; k<n ; ++k) {
+        printf("%d\n",arr[k]);
     }
-
     return 0;
 }
+
